d3ac1354b372e653692017351a83689e5908892a
Fatema Jamil <fatemajamil.wd@gmail.com>
2025-10-24 19:18:21 +0600

    textfield2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:myfirstapp/textfield-2.dart';
 import 'package:myfirstapp/textfiled-1.dart';
 
 import 'gridview.dart';
@@ -11,7 +12,7 @@ void main(){
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TextFieldScreen(),
+      home:FormScreen(),
     )
   );
 }
